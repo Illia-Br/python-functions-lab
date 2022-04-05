c0dbc0e08ed32b913d1396aaffe5319d58dff687
Illia Brytun <elbrytun@gmail.com>
2022-04-05 17:30:31 -0400

    solve task 3

--- python-functions.py
+++ python-functions.py
@@ -11,3 +11,16 @@ def largest(li):
 
 # print(largest([1,289, 2, 3, 4, 0, 289, 278]))
 
+def occurences(str_1, str_2):
+  counter = 0
+  if len(str_2) == 1:
+    for character in str_1:
+      if str_2 in character:
+        counter += 1
+  else:
+    for word in str_1.split():
+          if str_2 in word:
+            counter += 1
+  return counter
+
+# print(occurences('fleep floop', 'ee'))
